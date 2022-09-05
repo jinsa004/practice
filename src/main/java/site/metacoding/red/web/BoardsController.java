aee80f7588f2229c8808a760458329c0dec052db
jinsa <jinsa004@gmail.com>
2022-09-05 09:51:47 +0900

    글쓰기 인증 체크완료

--- src/main/java/site/metacoding/red/web/BoardsController.java
+++ src/main/java/site/metacoding/red/web/BoardsController.java
@@ -1,28 +1,40 @@
 package site.metacoding.red.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.red.domain.users.Users;
+
+@RequiredArgsConstructor
 @Controller
 public class BoardsController {
 
-	//@PostMapping("/boards/{id}/delete")
-	//@PostMapping("/boards/{id}/update")
-	
-	@GetMapping({"/","/boards"})
+	private final HttpSession session; // 스프링이 서버시작시에 IoC 컨테이너에 보관함.
+	// @PostMapping("/boards/{id}/delete")
+	// @PostMapping("/boards/{id}/update")
+
+	@GetMapping({ "/", "/boards" })
 	public String getBoardList() {
 		return "boards/main";
 	}
-	
+
 	@GetMapping("/boards/{id}")
 	public String getBoardList(@PathVariable Integer id) {
 		return "boards/detail";
 	}
-	
+
 	@GetMapping("/boards/writeForm")
 	public String writeForm() {
-		return "boards/writeForm";
+		Users principal = (Users) session.getAttribute("principal");
+		if (principal == null) {
+			return "redirect:/loginForm";
+		} else {
+			return "boards/writeForm";
+		}
+
 	}
 }
